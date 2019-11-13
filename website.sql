50a00e69327c516d0af4622ce64e46f475a0ca5d
alid12 <alid12@coventry.ac.uk>
2019-11-13 22:18:49 +0000

    ADDING IN NEW DATABSE                                                                     Altered the field names in sqlite quries to match database and reachecked any databse reference                                                                        to avoid future errors when extracting data

--- website.sql
+++ website.sql
@@ -1,5 +1,5 @@
 
-INSERT into Lecture (Id, Title, Text, links) VALUES(1, "<h1>Why using GIT?</h1>","<img class='gitlogo' src='images/gitlogo.jpg'>
+INSERT INTO lecture (id, title, text, module_id) VALUES(1, "<h1>Why using GIT?</h1>","<img class='gitlogo' src='images/gitlogo.jpg'>
 
                     <p>If you have already worked on a programming project in group, you probably have encountered problems such as modifications by someone,
                     but you don’t know who, or the impossibility to work on the same code at the same time. All these problems can be solved 
@@ -82,11 +82,9 @@ INSERT into Lecture (Id, Title, Text, links) VALUES(1, "<h1>Why using GIT?</h1>"
                     <p>Git is a must-have tool in a programming project, it has a lot of game-changing functionalities. A particularity of Git is the 
                     existence of collaborative website such as GitHub. This is a social media for developers, with a huge community. This is an excellent way
                     to participate in projects and ask for help.</p>",
-                    "<a href=https://biz30.timedoctor.com/git-mecurial-and-cvs-comparison-of-svn-software>Version-Control system Comparison</a><br>
-                     <a href=https://www.youtube.com/watch?v=SWYqp7iY_Tc>Getting started with Git</iframe><br>
-                     <a href=https://www.atlassian.com/git/tutorials/why-git>Technical description of Git</a>");
+                      1);
 
-INSERT INTO lecture (id, title, text, module_name) 
+INSERT INTO lecture (id, title, text, module_id)
     VALUES(
          5, 
         "<h1>Branches</h1>", 
@@ -132,9 +130,9 @@ INSERT INTO lecture (id, title, text, module_name)
                        </figcaption>
                        </figure>
                    <p>You created and switched to a branch, did some work on it, and then switched back to your main branch and did other work. Both of those changes are isolated in separate branches: you can switch back and forth between the branches and merge (explained in the next lesson) them together when you’re ready. And you did all that with simple <code>branch</code> and <code>checkout</code> commands.</p>",
-         "Git"
+         "1"
     );
-INSERT into Lecture (Id, Title, Text, links) VALUES(3, "<h1>test</h1>","<img class='gitlogo' src='images/gitlogo.jpg'>
+INSERT into lecture (id, title, text, module_id) VALUES(3, "<h1>test</h1>","<img class='gitlogo' src='images/gitlogo.jpg'>
 
                     <p>Test.</p>
 
