c1563ace897e5bf7f50761384245c6597f7ecf95
lalym <lalym@coventry.ac.uk>
2019-11-13 00:45:35 +0000

    Changes: Added Login Page Added Register Page username/ password checker. Added logout button to delete session. Only logged in users allowed to connect to menu page & lecture 1 & 2.

--- index.js
+++ index.js
@@ -53,63 +53,130 @@ router.get('/', async ctx => {
 		await ctx.render('error', {message: err.message})
 	}
 })
-
+router.get('/Lecture/1', async ctx => {
+	try {
+		if(ctx.session.authorised !== true) return ctx.redirect('/login?msg=you need to log in')
+		const data = {}
+		if(ctx.query.msg) data.msg = ctx.query.msg
+		await ctx.render('Menu')
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+})
+router.get('/Lecture/2', async ctx => {
+	try {
+		if(ctx.session.authorised !== true) return ctx.redirect('/login?msg=you need to log in')
+		const data = {}
+		if(ctx.query.msg) data.msg = ctx.query.msg
+		await ctx.render('Menu')
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+})
 /**
  * The user registration page.
  *
  * @name Register Page
  * @route {GET} /register
  */
-router.get('/register', async ctx => await ctx.render('register'))
-
-/**
+router.get('/register', async ctx =>  {
+	const data = {}
+	if(ctx.query.msg) data.msg = ctx.query.msg
+	await ctx.render('register', data)
+ })
+  
+ /**
  * The script to process new user registrations.
  *
  * @name Register Script
  * @route {POST} /register
  */
-router.post('/register', koaBody, async ctx => {
+ router.post('/register', koaBody, async ctx => {
 	try {
-		// extract the data from the request
 		const body = ctx.request.body
 		console.log(body)
+		// PROCESSING FILE
 		const {path, type} = ctx.request.files.avatar
-		// call the functions in the module
-		const user = await new User(dbName)
-		await user.register(body.user, body.pass)
-		// await user.uploadPicture(path, type)
-		// redirect to the home page
-		ctx.redirect(`/?msg=new user "${body.name}" added`)
+		const fileExtension = mime.extension(type)
+		console.log(`path: ${path}`)
+		console.log(`type: ${type}`)
+		console.log(`fileExtension: ${fileExtension}`)
+		await fs.copy(path, 'public/avatars/avatar11.png')
+		// USERNAME AND PASSWORD BLANK CHECKER
+		var x = body.user;
+		var y = body.pass;
+		var letters = /^[A-Za-z]+$/;
+		// CHECKS IF USERNAME AND PASSWORD BOX CONTAINS ONLY LETTERS
+		if (x.match(letters) && y.match(letters))
+		{
+			// DOES THE USERNAME EXIST IN DATABASE
+			const db = await sqlite.open('./website.db')
+			const userChecker = await db.get(`SELECT user FROM users WHERE user="${body.user}";`)
+			if (!userChecker)
+			{
+				// ENCRYPTING PASSWORD AND BUILDING SQL
+				body.pass = await bcrypt.hash(body.pass, saltRounds)
+				const sql = `INSERT INTO users(user, pass) VALUES("${body.user}", "${body.pass}")`
+				console.log(sql)
+				// DATABASE COMMANDS
+				await db.run(sql)
+				await db.close()
+				// REDIRECTING USER TO HOME PAGE
+				ctx.redirect(`/login`)
+			} else
+			{
+				return ctx.redirect('/register?msg=The username has been taken.')
+			}
+		} else
+		{
+			return ctx.redirect('/register?msg=The username and password box has to contain a value.')
+		}
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
 	}
-})
-
-router.get('/login', async ctx => {
+ })
+  
+ router.get('/login', async ctx => {
 	const data = {}
 	if(ctx.query.msg) data.msg = ctx.query.msg
 	if(ctx.query.user) data.user = ctx.query.user
 	await ctx.render('login', data)
-})
-
-router.post('/login', async ctx => {
+ })
+  
+ router.post('/login', async ctx => {
 	try {
 		const body = ctx.request.body
-		const user = await new User(dbName)
-		await user.login(body.user, body.pass)
+		const db = await sqlite.open('./website.db')
+		// DOES THE USERNAME EXIST?
+		const records = await db.get(`SELECT user FROM users WHERE user="${body.user}";`)
+		if(!records) return ctx.redirect('/login?msg=invalid%20username')
+		const record = await db.get(`SELECT pass FROM users WHERE user = "${body.user}";`)
+		await db.close()
+		// DOES THE PASSWORD MATCH?
+		const valid = await bcrypt.compare(body.pass, record.pass)
+		if(valid == false) return ctx.redirect(`/login?user=${body.user}&msg=invalid%20password`)
+		// WE HAVE A VALID USERNAME AND PASSWORD
 		ctx.session.authorised = true
-		return ctx.redirect('/?msg=you are now logged in...')
+		return ctx.redirect('/')
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
 	}
-})
-
-router.get('/logout', async ctx => {
+ })
+  
+ router.get('/logout', async ctx => {
 	ctx.session.authorised = null
-	ctx.redirect('/?msg=you are now logged out')
-})
-
-
+	ctx.redirect('/?msg=you have logged out successfully')
+ })
+  
+ router.post('/logout', async ctx => {
+	try {
+		ctx.session.authorised = null;
+		return ctx.redirect('/login?msg=you have logged out successfully')
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+ })
+ 
 /* Lecture */
 
 
