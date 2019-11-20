edc3d7f7e1da1d563e5445ba3d2d9b8ac885ebe8
Hamza Javed (javedh5) <javedh5@coventry.ac.uk>
2019-11-20 18:13:40 +0000

    added quiz for lecture 3

--- quiz.sql
+++ quiz.sql
@@ -52,9 +52,61 @@ INSERT INTO question (id, question , lecture_id) VALUES (
                                     10,
                                     "10) Which kind of version-control is GIT?",
                                     1);
+                                    
+                                    
+                                    
 
 
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    1,
+                                    "1) It is recommended to create a repository…",
+                                    3);
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    2,
+                                    "2) The git command used to create a new empty repository is…",
+                                    3);
 
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    3,
+                                    "3) Which command is used to stage files before a commit?",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    4,
+                                    "4) Which of these shows the correct syntax for committing changes along with a brief message?",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    5,
+                                    "5) The git init command will have the same effect regardless of whether it is used at the start, middle or end of a project.",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    6,
+                                    "6) gitignore. is used to prevent unwanted files from being added to the repository.",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    7,
+                                    "7) The git init command results in the creation of a new master branch.",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    8,
+                                    "8) Dependency caches (e.g. node modules) must always be committed to the repository.",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    9,
+                                    "9) Which of these shows the correct sequence of commands needed to successfully commit a file to the repository?",
+                                    3);
+
+INSERT INTO question (id, question , lecture_id) VALUES ( 
+                                    10,
+                                    "10) If the git init command is used while a repository is already present…",
+                                    3);
+                                    
+                                    
 
 
 
