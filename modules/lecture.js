cac3151c099d57e0164dcf7779eea0d89d830fa9
alid12 <alid12@coventry.ac.uk>
2019-11-27 00:15:05 +0000

    Throw Errors and alter code logic
    added error messaged to be thrown when missing fields are added to the databse. Added the module ID to getlecture
    
    To prevent addition of lectures with missing information as their content. added module id parameter to get lecture to specifically get the lecture depending on which module the user is on: GIT or HTML

--- modules/lecture.js
+++ modules/lecture.js
@@ -14,9 +14,13 @@ module.exports = class Lecture {
 			return this
 		})()
 	}
-
+	 // Insert a lecture with its ID,title, text and module ID into the databse
 	async addlecture(id, title, text, moduleId) {
 		try {
+			if(id.toString().length === 0) throw new Error('missing lecture id')
+			if(title.length === 0) throw new Error('missing lecture title')
+			if(text.length === 0) throw new Error('missing lecture text')
+			if(moduleId.toString().length === 0) throw new Error('missing module id')
 			const data=await this.db.get(`INSERT INTO lecture (id, title, text,module_id)
                                                 VALUES (${id},"${title}","${text}",${moduleId});`)
 			return data
@@ -24,27 +28,32 @@ module.exports = class Lecture {
 			throw err
 		}
 	}
-	async getlecture(id) {
+	// get the chosen lecture of a specific module 
+	async getlecture(id,moduleId) {
 		try {
 			const data=await this.db.get(`SELECT id, title, text,module_id FROM lecture
-                                                WHERE id =${id};`)
+												WHERE id =${id}
+												AND module_id=${moduleId};`)
 			return data
 		} catch(err) {
 			throw err
 		}
 	}
-
+    // delete the chosen lecture of a specific module 
 	async deletelecture(id, moduleId) {
 		try {
-			await this.db.get(`DELETE FROM lecture WHERE id=${id} AND module_id=${moduleId};`)
+			await this.db.get(`DELETE FROM lecture
+										 WHERE id=${id} 
+										 AND module_id=${moduleId};`)
 			console.log("delete")
 		} catch(err) {
 			throw err
 		}
 	}
+	// update the chosen lecture of a specific module 
 	async updatelecture(id, title, text, moduleId) {
 		try {
-			await this.db.get(`UPDATE lecture SET title="${title}",text="${text}" 
+			await this.db.get(`UPDATE lecture SET title="${title}",text="${text}"
                                                         WHERE id=${id}
                                                         AND module_id=${moduleId};`)
 		} catch(err) {
